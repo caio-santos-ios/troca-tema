23fa54b712c567db8df2eef3fdb4de3929fca88f
caiosantos.dev@outlook.com <seu@email.com>
2024-02-23 17:12:52 -0300

    corrigindo tamanho btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import './App.css'
 import { Button } from './components/Button';
-import photoNigth from "./assets/2410140-rio-a-noite-paisagem-fundo-gratis-vetor.jpg";
-import photoSun from "./assets/depositphotos_650649572-stock-photo-peaceful-meadow-atop-hill-aglow.jpg";
 import { nigthJotai } from './jotai';
 import { useAtom } from 'jotai';
 
@@ -11,7 +9,6 @@ function App() {
 
   return (
     <main>
-      <img src={ nigth ? photoNigth : photoSun } alt="foto-de-paisagem" />
       <div className='container'>
           <Button />
       </div>
