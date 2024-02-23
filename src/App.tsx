dc0645f52991690d2d11b46fcfb058e6db4504d3
caiosantos.dev@outlook.com <seu@email.com>
2024-02-23 17:13:58 -0300

    corrigindo tamanho btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import './App.css'
+import './App.css';
 import { Button } from './components/Button';
-import { nigthJotai } from './jotai';
-import { useAtom } from 'jotai';
-
 
 function App() {
-  const [nigth] = useAtom(nigthJotai)
 
   return (
     <main>
