0ac2680d65b4f8a1a2304a0c952486325838faeb
caiosantos.dev@outlook.com <seu@email.com>
2024-02-26 15:35:18 -0300

    colocando fundo branco e preto

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useAtom } from 'jotai';
 import './App.css';
 import { Button } from './components/Button';
+import { nigthJotai } from './jotai';
+import { useEffect } from 'react';
 
 function App() {
+  const [nigth] = useAtom(nigthJotai)
+
+  useEffect(() => {
+    const body = document.querySelector("body")
+    if(nigth) {
+      body.style.background = 'black'
+    } else {
+      body.style.background = 'white'
+    }
+  }, [nigth])
 
   return (
     <main>
